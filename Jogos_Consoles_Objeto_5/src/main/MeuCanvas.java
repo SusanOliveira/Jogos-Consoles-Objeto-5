51cd107f9d62fa5d351bc6f06cec09f08912405e
JohnFonseca <81266870+JohnFonseca@users.noreply.github.com>
2021-05-17 23:22:28 -0300

    Update MeuCanvas.java
    Implementação de um canvas para que as particulas possam ser demonstradas

--- Jogos_Consoles_Objeto_5/src/main/MeuCanvas.java
+++ Jogos_Consoles_Objeto_5/src/main/MeuCanvas.java
@@ -1,7 +1,49 @@
 package main;
 
-import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MeuCanvas extends Canvas{
+import javax.swing.JPanel;
 
-}
+public class MeuCanvas extends JPanel implements ActionListener{
+
+	ParticulaCirculo particula[];
+	public MeuCanvas(int quantidadeDeParticulas)
+	{
+		particula = new ParticulaCirculo[quantidadeDeParticulas];
+	}
+	
+	public void EnvioDeParticulas(ParticulaCirculo[] particulas)
+	{
+		 particula = particulas;
+		 
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	protected void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paintComponent(g);
+		this.setBackground(new Color(255,255,255));
+		
+		if(particula!=null)
+		{
+			for (int i = 0; i < particula.length; i++) {
+				particula[i].paint(g);
+				
+			}
+		}
+
+				
+		}
+		
+		
+	}
+
+
