d0dd3da150bd1bd39b90183f9e4910802b9a6f51
JohnFonseca <81266870+JohnFonseca@users.noreply.github.com>
2021-05-18 16:09:54 -0300

    Revert "Alteração no Canvas"
    This reverts commit 5b923bc82d55e91771f3b1c43413dfef5812e489.

--- Jogos_Consoles_Objeto_5/src/main/MeuCanvas.java
+++ Jogos_Consoles_Objeto_5/src/main/MeuCanvas.java
@@ -10,7 +10,6 @@ import javax.swing.JPanel;
 public class MeuCanvas extends JPanel implements ActionListener{
 
 	ParticulaCirculo particula[];
-	QuadTree quadtree;
 	public MeuCanvas(int quantidadeDeParticulas)
 	{
 		particula = new ParticulaCirculo[quantidadeDeParticulas];
@@ -20,10 +19,6 @@ public class MeuCanvas extends JPanel implements ActionListener{
 	{
 		 particula = particulas;
 		 
-	}
-	public void EnvioDaQuadTree(QuadTree valor)
-	{
-		quadtree = valor;
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -36,12 +31,8 @@ public class MeuCanvas extends JPanel implements ActionListener{
 		// TODO Auto-generated method stub
 		super.paintComponent(g);
 		this.setBackground(new Color(255,255,255));
-		if(MainClass.quadTreeBoolean==false)
-		g.drawRect(0, 0, MainClass.tamanhoX, MainClass.tamanhoY);
-		if(quadtree!=null)
-		quadtree.paint(g);
 		
-		if(particula[0]!=null)
+		if(particula!=null)
 		{
 			for (int i = 0; i < particula.length; i++) {
 				particula[i].paint(g);
